e2c86081ece32bf3f297952d682f1d9aeab6915b
Stanley Lew <stanley43595@gmail.com>
2023-12-21 13:02:52 -0800

    export pt. 7

--- next.config.js
+++ next.config.js
@@ -1,7 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    outputDirectory: 'out',
+    distDir: 'dist',
+    images:{
+        unoptimized: true,
+    }
 };
 
 module.exports = nextConfig
