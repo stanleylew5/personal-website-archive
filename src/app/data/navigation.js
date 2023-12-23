096ef7a4a558fd22e30f5b6cfd892d2ed7b91fb5
Stanley Lew <stanley43595@gmail.com>
2023-12-23 02:45:10 -0800

    added nav files + shadow on pfp

---
+++ src/app/data/navigation.js
@@ -0,0 +1,26 @@
+export const navigation = [
+    {
+        text: "HOME",
+        link: "/#home",
+    },
+    {
+        text: "ABOUT",
+        link: "/#about",
+    },
+    {
+        text: "SKILLS",
+        link: "/#skills",
+    },
+    {
+        text: "PROJECTS",
+        link: "/#projects",
+    },
+    {
+        text: "RESUME",
+        link: "/#resume",
+    },
+    {
+        text: "CONTACT",
+        link: "/#contact",
+    },
+];
