aa6410542183135b43afadee0e327a67e2944dc8
Stanley Lew <stanley43595@gmail.com>
2024-08-27 12:13:48 -0700

    contacts

--- dist/static/chunks/webpack.js
+++ dist/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "55199e11c0e4c4a4"; }
+/******/ 		__webpack_require__.h = function() { return "14d2aafe6e611e4c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -6,6 +6,7 @@ import ProjectLeft from "./ProjectLeft.jsx";
 import ProjectRight from "./ProjectRight.jsx";
 import { projects } from "../data/projects.js";
 import useMediaQuery from "../hooks/useMediaQuery.jsx";
+import { motion } from "framer-motion";
 
 const Projects = () => {
   const isMdOrLarger = useMediaQuery("(min-width: 768px)"); // md breakpoint is 768px
@@ -13,7 +14,7 @@ const Projects = () => {
   return (
     <div className="bg-stanley-gray-400">
       <Image src={hill3} alt="hill3" className="w-full" />
-      <div className="pb-[8vh]">
+      <div className="pb-[8vh]" id="projects">
         <Title text={"Projects"} />
       </div>
       <div>
@@ -24,15 +25,24 @@ const Projects = () => {
               : ProjectRight
             : ProjectLeft;
           return (
-            <ProjectComponent
-              key={index}
-              image={<Image src={project.image} alt={project.name} />}
-              name={project.name}
-              description={project.description}
-              website={project.website}
-              github={project.github}
-              icons={project.icons.map((Icon, idx) => <Icon key={idx} size={24} />)}
-            />
+            <motion.div 
+            initial={{ opacity: 0, y: 30 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            transition={{
+              type: "tween",
+              duration: 0.5,
+              delay: (index % 5) * 0.2,
+            }}
+            key={index}>
+              <ProjectComponent
+                image={<Image src={project.image} alt={project.name} />}
+                name={project.name}
+                description={project.description}
+                website={project.website}
+                github={project.github}
+                icons={project.icons.map((Icon, idx) => <Icon key={idx} size={24} />)}
+              />
+            </motion.div>
           );
         })}
       </div>
